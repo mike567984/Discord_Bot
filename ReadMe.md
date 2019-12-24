c104b776b54a95c17d3cce515f69c7855158cd9b
Max xu <mike567984@gmail.com>
2019-12-24 14:05:57 -0500

    Added image command to search for images of certain subject

--- ReadMe.md
+++ ReadMe.md
@@ -1,11 +1,15 @@
 Disclaimer: This bot only works if I have Admin perms on the specific server\
 How to start bot (bash):token = "your token" node index.js\
 Prefix: All bot commands need to start with a "!"\
-Learn all commands with !Commands\
+Learn all commands with !Commands
 
 Features:\
 Commands: Private messages the person about the commands available\
 Profile : Tells you about the users\
+Pictures : Currently searches and shows random pictures of cute animals (can be set to other subjects manually)
 Compliment: Gives you a random compliment\
 Delete(Still need to check perms): Deletes x number of messages based on user input\
+Status: Can change the bot to "Playing" x , "Streaming" x, "Listening to" x\
+
+
 -Bot also Welcomes anyone who joins the server
--- index.js
+++ index.js
@@ -8,13 +8,37 @@ const token = process.env.token;
 const botstart = "!";
 //package.json for variables
 const pkg = require('./package.json');
-//when bot in ready say this msg
-clbot.on('ready', () => {
-  console.log("It's on boy")
-  //Sets bot to do something
-  clbot.user.setActivity('Just Vibing')
-})
+//Cheerio package for images
+const cheerio = require('cheerio');
+//request package for images
+const request = require('request');
 
+function image(msg) {
+//Uses get request to find images for certain search result
+  var options = {
+    url: "http://results.dogpile.com/serp?qc=images&q=" + "Cute Animals",
+    method: "GET",
+    headers: {
+      "Accept": "text/html",
+      "User-Agent": "Chrome"
+    }
+  };
+//request function to parse data from options
+  request(options, function(error, response, responseBody) {
+    if (error) {
+      return;
+    }
+    $ = cheerio.load(responseBody);
+    var links = $(".image a.link");
+    var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+    console.log(urls);
+    if (!urls.length) {
+      return;
+    }
+    //randomizes results for the same subject search
+    msg.channel.send(urls[Math.floor(Math.random() * urls.length)]);
+  });
+}
 
 //an array of compliments now for anyone who asked for it
 let compliments = ["you'r so cool", "you'r so handsome", "End me", "you'r ok", "you'r so smart", "Eveything I said before was a lie"]
@@ -28,10 +52,16 @@ function randoNum(myMin) {
 function rnum(myMin) {
   return Math.floor(Math.random() * (500 - myMin + 1)) + myMin;
 }
+//when bot in ready say this msg
+clbot.on('ready', () => {
+  console.log("It's on boy")
+  //Sets bot to do something
+  clbot.user.setActivity('Just Vibing')
+})
 
 //If you type in "Compliment" in discord it will respond with a random compliment from array
 clbot.on('message', msg => {
-  // !compliment and not case sensative anymore
+  //Gives you random compliment from compliments array
   if (msg.content.toLowerCase() == botstart + "compliment") {
     msg.reply(compliments[randoNum(0)]);
   }
@@ -41,23 +71,23 @@ clbot.on('message', msg => {
 clbot.on('message', msg => {
   const user = msg.author;
   if (!user.bot) {
-    //breaks message apart so i can have 2 or more args in a command
+    //breaks message apart so you can have 2 or more args in a command
     let dprefix = msg.content.toLowerCase().substring(botstart.length).split(" ");
     //Checks to see if msg has ! in front of it
     switch (dprefix[0]) {
-        case 'commands':
-        case 'command':
-        case 'help':
+      case 'commands':
+      case 'command':
+      case 'help':
         //Pm's person about all commands avalible
         const helper_Pm = new Discord.RichEmbed()
           .setTitle("All avalible commands")
           .setColor(0xF000FF)
-          .setDescription(' Every bot command needs a ! before it\n !Author = tells you about the cutest man alive \n !Compliment = Compliments U\n !profile=Shows your profile ');
+          .setDescription(' Every bot command needs a ! before it\n !Author = tells you about the cutest man alive \n !Compliment = Compliments U\n !profile=Shows your profile \n !image = Show you a random image of a subject (currently set to cute animlas)');
         msg.author.send(helper_Pm);
         break;
         //Deletes a certain amount of messages given by user
       case 'delete':
-      let Admrole = msg.guild.roles.find(role => role.name === 'Admin');
+        let Admrole = msg.guild.roles.find(role => role.name === 'Admin');
         if (dprefix[1]) {
           msg.channel.bulkDelete(dprefix[1]);
           msg.channel.send("Deleted " + dprefix[1] + " Messages")
@@ -66,7 +96,11 @@ clbot.on('message', msg => {
         }
         break;
 
-
+      case 'image':
+      case 'pic':
+      case 'pictures'
+        image(msg);
+        break;
 
         //A fun get to know
       case 'author':
@@ -92,6 +126,7 @@ clbot.on('message', msg => {
 clbot.on('guildMemberAdd', member => {
   member.guild.channels.get('658808475551531059').send("Welcome To the server");
 })
+
 
 
 
