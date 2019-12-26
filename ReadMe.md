fb16ef997363c0301dc2bad0e662ebeb9e582ec4
Max xu <mike567984@gmail.com>
2019-12-25 20:26:53 -0500

    Added dance and Christmas command and added gif to welcome message

--- ReadMe.md
+++ ReadMe.md
@@ -3,13 +3,15 @@ How to start bot (bash):token = "your token" node index.js\
 Prefix: All bot commands need to start with a "!"\
 Learn all commands with !Commands
 
-Features:\
+Active Features:\
 Commands: Private messages the person about the commands available\
 Profile : Tells you about the users\
-Pictures : Currently searches and shows random pictures of cute animals (can be set to other subjects manually)
+Pictures : Currently searches and shows random pictures of any subject of choice (can be set to other subjects manually)
 Compliment: Gives you a random compliment\
 Delete(Still need to check perms): Deletes x number of messages based on user input\
+Attachments / Dance: Can send any attachments on the web\
 Status: Can change the bot to "Playing" x , "Streaming" x, "Listening to" x
+Christmas: Celebrate the holidays
 
-
--Bot also Welcomes anyone who joins the server
+Passive Features:\
+-Bot also Welcomes anyone who joins the server with a gif of welcome and their name
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-//installed the discord.js lib
-const Discord = require('discord.js');
-//crete a new object aka the bot
-const clbot = new Discord.Client();
+require('discord.js');
+const {
+  Client,
+  Attachment
+} = require('discord.js');
+
+var Discord = require('discord.js');
+
+const clbot = new Client();
+
 //token for logging into bot
 const token = process.env.token;
 //need the ! so the bot knows you'r talking to it
@@ -14,16 +20,16 @@ const cheerio = require('cheerio');
 const request = require('request');
 
 function image(msg) {
-//Uses get request to find images for certain search result
+  //Uses get request to find images for certain search result
   var options = {
-    url: "http://results.dogpile.com/serp?qc=images&q=" + "Cute Animals",
+    url: "http://results.dogpile.com/serp?qc=images&q=" + "Anime",
     method: "GET",
     headers: {
       "Accept": "text/html",
       "User-Agent": "Chrome"
     }
   };
-//request function to parse data from options
+  //request function to parse data from options
   request(options, function(error, response, responseBody) {
     if (error) {
       return;
@@ -39,22 +45,32 @@ function image(msg) {
     msg.channel.send(urls[Math.floor(Math.random() * urls.length)]);
   });
 }
-
+// An array of Dance gifs from giphly
+let dance = ['https://media.giphy.com/media/W6dHvprT7oks6BpX5R/giphy.gif', 'https://media.giphy.com/media/11lxCeKo6cHkJy/giphy.gif', 'https://media.giphy.com/media/3o7bul4bNw60uhhQyI/giphy.gif', 'https://media.giphy.com/media/a6pzK009rlCak/giphy.gif',
+  'https://media.giphy.com/media/Kx0vubGB5nHCE/giphy.gif', 'https://media.giphy.com/media/LQo79CTiVpwwYYC14Q/giphy.gif', 'https://media.giphy.com/media/TI9HiyUqRm75jPyKQ5/giphy.gif'
+]
+const dnce = dance.length;
 //an array of compliments now for anyone who asked for it
 let compliments = ["you'r so cool", "you'r so handsome", "End me", "you'r ok", "you'r so smart", "Eveything I said before was a lie"]
 //var to keep track of size of compliments array
 const size = compliments.length;
+
 //Created this random function to give a diffrent Compliment each time for the Compliment FUnction
 function randoNum(myMin) {
   return Math.floor(Math.random() * (size - myMin + 1)) + myMin;
 }
+//dance gif
+function dran(myMin) {
+  return Math.floor(Math.random() * (dnce - myMin + 1)) + myMin;
+}
+
 //lucky Number generator
 function rnum(myMin) {
   return Math.floor(Math.random() * (500 - myMin + 1)) + myMin;
 }
 //when bot in ready say this msg
 clbot.on('ready', () => {
-  console.log("It's on boy")
+  console.log("It's online bud")
   //Sets bot to do something
   clbot.user.setActivity('Just Vibing')
 })
@@ -82,7 +98,7 @@ clbot.on('message', msg => {
         const helper_Pm = new Discord.RichEmbed()
           .setTitle("All avalible commands")
           .setColor(0xF000FF)
-          .setDescription(' Every bot command needs a ! before it\n !Author = tells you about the cutest man alive \n !Compliment = Compliments U\n !profile=Shows your profile \n !image = Show you a random image of a subject (currently set to Cute Animals)');
+          .setDescription(' Every bot command needs a ! before it\n !Author = tells you about the cutest man alive \n !Compliment = Compliments U\n !profile=Shows your profile \n !image = Shows you a random image of a subject (currently set to Anime)\n !dance=Dance Party \n !christmas = Merry Christmas to all');
         msg.author.send(helper_Pm);
         break;
         //Deletes a certain amount of messages given by user
@@ -108,7 +124,7 @@ clbot.on('message', msg => {
         break;
         //Info about the person
       case 'profile':
-        const profiles = new Discord.RichEmbed()
+        var profiles = new Discord.RichEmbed()
           .addField('Welcome to ', msg.guild.name)
           .addField('Profile name', user.username)
           .attachFile('welcome.jpg')
@@ -118,13 +134,32 @@ clbot.on('message', msg => {
         //.setAuthor(user.username, user.avatar_url)
         msg.channel.send(profiles);
         break;
+        //It's that time of year
+      case 'christmas':
+        var profiles = new Discord.RichEmbed()
+        .addField('Merry Christmas ', user.username)
+        .setThumbnail(user.avatarURL)
+        .attachFile('https://media.giphy.com/media/R7AW255ijTdV6/giphy.gif')
+        .setColor('EA4630')
+        msg.channel.send(profiles);
+        break;
+        //Dance party Gifs
+      case 'dance':
+        const attachment = new Attachment(dance[dran(0)])
+        console.log(attachment)
+        msg.channel.send("It's dancing Time " + msg.author, attachment);
+        break;
 
     }
   }
 })
-//Sends a message to those who oined the server
+//Sends a message to those who joined the server (Chanel has to be named welcome)
 clbot.on('guildMemberAdd', member => {
-  member.guild.channels.get('658808475551531059').send("Welcome To the server");
+  const channel = member.guild.channels.find(channel => channel.name === 'welcome');
+  if (!channel) return;
+  const attachment = new Attachment('https://media.giphy.com/media/xUPGGDNsLvqsBOhuU0/giphy.gif')
+  channel.send(attachment);
+  channel.send(`Welcome to the server , ${member} , enjoy your stay!`);
 })
 
 
